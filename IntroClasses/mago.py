5723c4c10f98794a3eb1a99993eac9d153678954
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2023-03-14 22:10:57 -0300

    Update mago.py

--- IntroClasses/mago.py
+++ IntroClasses/mago.py
@@ -1,3 +1,5 @@
+# Definição da classe Mago 
+
 class Mago:
     def __init__(self, nome, idade, escola):
         # Atributos de instância
@@ -15,13 +17,18 @@ class Mago:
         print('Invocando magia!')
         
         
-
+#Intanciação de um objeto da classe Mago
 hp = Mago('Harry Potter', 17, 'Hogwarts')
+gd = Mago('Gandalf', 2000, 'Magia Cinza')
 
 print(hp.nome)
 print(hp.idade)
 print(hp.escola)
 
+print(gd.escola)
+
 hp.andar()
 hp.falar()
 hp.invocarMagia()
+
+gd.falar()

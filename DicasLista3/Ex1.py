5be762991bb4e9c85461898e0bdf127625378d4e
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2023-04-11 22:27:00 -0300

    Adicionado Dicas Lista 3

---
+++ DicasLista3/Ex1.py
@@ -0,0 +1,94 @@
+class Pais:
+    def __init__(self,codigo=None,nome=None,dimensao=None):
+        self.__codigo = codigo
+        self.__nome = nome
+        self.__dimensao = dimensao
+        self.__populacao = 0
+        self.__vizinhos = []
+
+    def setCodigo(self,codigo):
+        self.__codigo = codigo
+
+    def getCodigo(self):
+        return self.__codigo    
+    
+    def compararIgual(self,pais):
+        #retorna true/false
+        if self.__codigo == pais.getCodigo():
+            return True
+        else:
+            return False
+
+    def verificarVizinho(self,pais):
+        for i in self.__vizinhos:
+            if i.compararIgual(pais) == True:
+                return True
+        return False
+
+    def calcularDensidade(self):
+        pass
+
+    #OBS.: este método está errado (em revisão)
+    def vizinhosComuns(self,pais):
+        vizinhosPais = pais.getVizinhos()
+        for i in self.__vizinhos:
+            for j in vizinhosPais:
+                print(i.getCodigo(),' ',j.getCodigo(), end=' ')
+                print(i.verificarVizinho(j))
+                #if i.verificarVizinho(j) == True:
+                    #print(j.getCodigo())
+
+
+    def adicionarVizinho(self,pais):
+        self.__vizinhos.append(pais)
+
+    def getVizinhos(self):
+        return self.__vizinhos
+    
+    def listarVizinhos(self):
+        for i in self.__vizinhos:
+            print(i.getCodigo())
+
+
+
+
+
+
+
+#####################
+
+pais01 = Pais('BRA','Brasil',100)
+
+pais02 = Pais()
+
+pais02.setCodigo('ARG')
+pais03 = Pais('URU','Uruguai',100)
+pais04 = Pais('PAR','Paraguai',100)
+pais05 = Pais('CHI','Chile',100)
+pais06 = Pais('PER','Peru',100)
+
+#adicionando vizinhos ao BRA
+pais01.adicionarVizinho(pais02)
+pais01.adicionarVizinho(pais03)
+pais01.adicionarVizinho(pais04)
+pais01.adicionarVizinho(pais06)
+
+#adicionando vizinhos a ARG
+
+pais02.adicionarVizinho(pais01)
+pais02.adicionarVizinho(pais03)
+pais02.adicionarVizinho(pais04)
+pais02.adicionarVizinho(pais05)
+
+print(pais01.getCodigo())
+print(pais02.getCodigo())
+
+print(pais01.compararIgual(pais02))
+
+pais01.listarVizinhos()
+pais02.listarVizinhos()
+# print(pais02.getVizinhos())
+
+print('-----------------')
+
+pais01.vizinhosComuns(pais02)
